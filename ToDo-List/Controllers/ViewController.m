86ee96644d85884deafc1b7b3eb54e5dd842c201
Igor Fernandes <igormedfer@gmail.com>
2022-12-16 13:34:30 -0300

    add bar buton item

--- ToDo-List/Controllers/ViewController.m
+++ ToDo-List/Controllers/ViewController.m
@@ -7,22 +7,35 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UITableViewDelegate, UITableViewDataSource>
 
 @property (strong, nonatomic) UITableView *tableView;
 
 @end
 
-@implementation ViewController
+@implementation ViewController 
+
+NSArray *names = @[@"Igor", @"Jessica", @"Caio"];
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    self.tableView = [[UITableView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height) style:UITableViewStylePlain];
+    _tableView = [[UITableView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height) style:UITableViewStylePlain];
     
-    self.tableView.dataSource = self;
-    self.tableView.delegate = self;
-    [self.view addSubview:self.tableView];
+    _tableView.dataSource = self;
+    _tableView.delegate = self;
+    [self.view addSubview:_tableView];
+    
+    UIButton *customButtom = [UIButton buttonWithType:UIButtonTypeCustom];
+    [customButtom setTitle:@"Add" forState:UIControlStateNormal];
+    [customButtom setTitleColor:UIColor.blueColor forState:UIControlStateNormal];
+    customButtom.frame = CGRectMake(0, 0, 50, 50);
+    
+    UIBarButtonItem *customButtonItem = [[UIBarButtonItem alloc] initWithCustomView:customButtom];
+    
+    self.navigationItem.rightBarButtonItem = customButtonItem;
+    
+   
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
@@ -30,12 +43,16 @@
     if (cell == nil) {
         cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"cell"];
     }
-    cell.textLabel.text = @"Hello World.";
+    cell.textLabel.text = names[indexPath.row];
     return cell;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return 3;
+    return names.count;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    [tableView deselectRowAtIndexPath: indexPath animated: true];
 }
 
 
